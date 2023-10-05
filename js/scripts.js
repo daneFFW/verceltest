e64f286c3142ff931afde25531690b95adf92419
daneFFW <dane.nagy@ffw.com>
2023-10-05 16:43:14 -0600

    reordered active group values

--- js/scripts.js
+++ js/scripts.js
@@ -132,7 +132,7 @@ function trackHandler(event){
     console.log("Consent Status:" + consent_status);
     managerProperties(event.target, data, user);
   
-    if(consent_status.includes("C0004:1,C0003:1")|consent_status.includes("C0004,C0003")){
+    if(consent_status.includes("C0003:1,C0004:1")|consent_status.includes("C0003,C0004")){
       analytics.track(event.target.dataset.event,data);
       analytics.identify(uuid,user);
       tab.insertAdjacentHTML("beforeend",`<div class="alertText"><p>Event: ${event.target.dataset.event}</p>
@@ -165,7 +165,7 @@ trackCalls.forEach((trackCall) => {
 document.onreadystatechange = () => {
   if(document.readyState=== "complete"){
     consent_status = checkConsent();
-  if(consent_status.includes("C0004:1,C0003:1")|consent_status.includes("C0004,C0003")){
+  if(consent_status.includes("C0003:1,C0004:1")|consent_status.includes("C0003,C0004")){
   analytics.page(document.title,{"consent_status":checkConsent()});
   console.log("DOM fully loaded and parsed Segment Pageview Called");
 }else{
